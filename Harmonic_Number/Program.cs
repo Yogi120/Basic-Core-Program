628576bda2b6f51e5754a8030c4cb3b3afcd67a2
Yogesh <me.yogesh.pal@gmail.com>
2023-06-14 19:35:23 +0530

    Harmonic Number Series

---
+++ Harmonic_Number/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Harmonic_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int count = 1; count <= number; count++)
+            {
+                Console.Write("1/{0} + ", count);
+            }
+        }
+    }
+}
