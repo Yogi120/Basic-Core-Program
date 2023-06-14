24e30a9549cfad94b0125d7e2add76dd12bde1b3
Yogesh <me.yogesh.pal@gmail.com>
2023-06-14 19:59:32 +0530

    Finding the power of 2

---
+++ PowerOf2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PowerOf2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number");
+            int user = Convert.ToInt32(Console.ReadLine());
+            int value = 2;
+            for (int power = 0; power <= user; power++)
+            {
+                if (user > 31)
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+                else
+                {
+                    Console.WriteLine($"{value}^{power} = {(long)Math.Pow(value, power):N0} (0x{(long)Math.Pow(value, power):X})");
+                }
+
+            }
+
+        }
+    }
+}
