109eeb8ebd6f05f177d92a98657d22bf430daf55
Yogesh <me.yogesh.pal@gmail.com>
2023-06-14 19:56:17 +0530

    Even and Odd

---
+++ Even_Odd/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Even_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("The number is Odd");
+            }
+        }
+    }
+}
