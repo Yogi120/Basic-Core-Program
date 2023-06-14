a898170c85d56c659db98739f84799c57f91c80f
Yogesh <me.yogesh.pal@gmail.com>
2023-06-14 19:38:20 +0530

    Getting Quotient and Remainder

---
+++ Quotient_Remainder/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Quotient_Remainder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int remainder;
+
+            Console.WriteLine("Enter the Dividen");
+            int dividend = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Divisor");
+            int divisor = int.Parse(Console.ReadLine());
+
+            int quotient = (dividend / divisor);
+            remainder = (dividend % divisor);
+
+
+            Console.WriteLine("Qutoient is: " + quotient );
+            Console.WriteLine("Remainder is: " + remainder);
+
+
+        }
+    }
+}
